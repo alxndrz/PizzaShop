7bb3862fd07d04ed4a1d504e05a34c657b7d4c6d
Alexandr <alexandr.oz_work@outlook.com>
2018-09-04 15:59:33 +0600

    Pass id to add_to_cart

--- public/app.js
+++ public/app.js
@@ -1,15 +1,10 @@
 function something() {
-
   var x = window.localStorage.getItem('bbb');
-
   x = x * 1 + 1;
-
   window.localStorage.setItem('bbb', x)
-
   alert(x);
 }
 
-function add_to_cart()
-{
-  alert('Hello from function!')
+function add_to_cart(id) {
+  alert('You added pizza with id: ' + id);
 }
