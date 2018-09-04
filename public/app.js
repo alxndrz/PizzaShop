ad901751e1bbb5da76cfce76cad9f4ba8283ebea
Alexandr <alexandr.oz_work@outlook.com>
2018-09-04 17:36:50 +0600

    Update add to cart

--- public/app.js
+++ public/app.js
@@ -1,10 +1,8 @@
-function something() {
-  var x = window.localStorage.getItem('bbb');
-  x = x * 1 + 1;
-  window.localStorage.setItem('bbb', x)
-  alert(x);
-}
 
-function add_to_cart(id) {
-  alert('You added pizza with id: ' + id);
+function add_to_cart(id) 
+{
+  var key = 'product_' + id;
+  var x = window.localStorage.getItem(key);
+  x = x * 1 + 1;
+  window.localStorage.setItem(key, x);
 }
